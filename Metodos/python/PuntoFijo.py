88ed6ef120febebc929f934d33037992a95b2d68
Emanuel Isaza <53051916+eisazav@users.noreply.github.com>
2022-05-27 16:22:50 -0500

    Add files via upload

---
+++ Metodos/python/PuntoFijo.py
@@ -0,0 +1,24 @@
+from python.EvaluadorFunciones import Evaluar
+from python.graf import grafica
+
+def PuntoFijo(x,e, n,funcion):
+    i = 1 
+    b = Evaluar(funcion,x)
+    error = abs(b-x)
+    Lista2=list()
+    while(error>=e and i<=n ):
+        a = b
+        b = Evaluar(funcion,a)
+        error = abs(b-a)
+        Lista2.append(b)
+        i = i + 1
+    plot=[(range(i-1),Lista2)]
+    graf=grafica(plot)
+    datos = {'x0' : a, 'x1' : b, 'Iteraciones' : i, 'error' : error}
+    if i >= n:
+        datos = f'Se excedio el numero de iteraciones {i}'
+        graf = False
+    return datos, graf 
+
+
+
---
+++ Metodos/python/gaussSeidel.py
@@ -0,0 +1,30 @@
+from numpy import zeros, ones, abs, max, array 
+def gaussSeidel(a, B, tol, iter):       
+    n = len(a)      
+    X=zeros(n)
+    a = array(a)
+    B = array(B)
+    diferencia = ones(n,dtype=float)
+    err = 2*tol
+    itera = 0
+    #stops if error is less than error or exceds iterations
+    while not(err<=tol or itera>iter):
+        for i in range(0,n,1):
+            suma=0
+            for j in range(0,n,1):
+                if(j!=i):
+                    suma=suma+a[i,j]*X[j]
+            nuevo=(B[i]-suma)/a[i,i]
+            diferencia[i]=abs(nuevo-X[i])
+            X[i]=nuevo
+        err = max(diferencia)
+        itera = itera+1               
+    respuesta = ''
+
+    if itera >= iter:
+         respuesta = "Se excedio el nuemro de iteraciones"
+    else:
+        for i in range(n):
+            respuesta +=  f'solution is:  X{i} = {X[i]:0.3f}\n' 
+
+    return respuesta  
